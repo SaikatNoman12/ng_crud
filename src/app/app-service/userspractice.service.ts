af703f921131343d428e76ca9b6549b30fe16877
saikatnoman12 <noommaa240@gmail.com>
2022-12-09 17:13:11 +0600

    update and edit user

--- src/app/app-service/userspractice.service.ts
+++ src/app/app-service/userspractice.service.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserPractice } from '../interface/user-practice.modal';
@@ -11,9 +12,43 @@ export class UserspracticeService {
 
   constructor(private http: HttpClient) { }
 
+  // set data in firebase use post method:-
   onSetData(userData: any) {
     return this.http.post<UserPractice>(this.postUrl, userData);
   }
 
+
+  // get data in firebase use get method:-
+  onGetData() {
+    return this.http.get<UserPractice>(this.postUrl)
+      .pipe(map(
+        (response: any) => {
+          const dataArr = [];
+          for (const key in response) {
+            if (response.hasOwnProperty(key)) {
+              dataArr.push({
+                userId: key,
+                ...response[key]
+              });
+            }
+          }
+          return dataArr;
+        }
+      ))
+  }
+
+  onEditData(userId:string, userData:any){
+    const updateUrl =`https://practice-6b131-default-rtdb.asia-southeast1.firebasedatabase.app/usersPractice/${userId}.json`; 
+
+    return this.http.put(updateUrl, userData);
+  }
+
+
+  // delete data in firebase use delete method:-
+  onDeleteData(userId:string){
+    const deleteUrl = `https://practice-6b131-default-rtdb.asia-southeast1.firebasedatabase.app/usersPractice/${userId}.json`;
+
+    return this.http.delete(deleteUrl);
+  }
 
 }
--- src/app/manage-user-practice/manage-user-practice.component.ts
+++ src/app/manage-user-practice/manage-user-practice.component.ts
@@ -11,8 +11,12 @@ import { UserspracticeService } from '../app-service/userspractice.service';
 export class ManageUserPracticeComponent implements OnInit {
 
   myRecForm!: FormGroup;
+  usersPractice: any;
 
-  usersPractice:any[] = [];
+  updateData: boolean = false;
+  userDataId: any;
+
+  spinnerShow: boolean = false;
 
   constructor(
     private _serverService: UserspracticeService
@@ -23,6 +27,8 @@ export class ManageUserPracticeComponent implements OnInit {
       'name': new FormControl(null, [Validators.required]),
       'technology': new FormControl(null, [Validators.required])
     });
+
+    this.myGetData();
   }
 
   // use for short validation:-
@@ -30,32 +36,76 @@ export class ManageUserPracticeComponent implements OnInit {
     return this.myRecForm.controls;
   }
 
+  // use set data:-
   myRecFromSubmit() {
-
-
     if (this.fc?.['name']?.value === null || this.fc?.['name']?.value === '' && this.fc?.['technology']?.value === null || this.fc?.['technology']?.value === '') {
       alert('please enter all input value!');
     }
-    else if(this.fc?.['name']?.value === null || this.fc?.['name']?.value === ''){
+    else if (this.fc?.['name']?.value === null || this.fc?.['name']?.value === '') {
       alert('Please enter name input value!');
     }
-    else if(this.fc?.['technology'].value === null || this.fc?.['technology']?.value === ''){
+    else if (this.fc?.['technology'].value === null || this.fc?.['technology']?.value === '') {
       alert('Please enter technology input value!');
     }
     else {
+      this.spinnerShow = true;
       const data: User = this.myRecForm.value;
-      this.usersPractice.push(data);
-      this._serverService.onSetData(data).subscribe(
-        (res) => {
-
-         },
-        (err) => { }
-      );
+      if (this.updateData) {
+        this._serverService.onEditData(this.userDataId, data).subscribe(
+          (res) => {
+            this.myGetData();
+            this.updateData = false;
+            this.spinnerShow = false;
+          },
+          (err) => { }
+        )
+      }
+      else {
+        this._serverService.onSetData(data).subscribe(
+          (res) => {
+            if (res !== null) {
+              this.myGetData();
+              this.spinnerShow = false;
+            }
+          },
+          (err) => { }
+        );
+      }
       this.myRecForm.reset();
-      console.log(this.myRecForm);
-
     }
+  }
 
+  // use fetch data:-
+  myGetData() {
+    this.spinnerShow = true;
+    this._serverService.onGetData().subscribe(
+      res => {
+        this.usersPractice = res;
+        this.spinnerShow = false;
+      }
+    );
+  }
+
+  myEditData(userId: string, index: number) {
+    this.updateData = true;
+
+    this.userDataId = userId;
+
+    const userData = this.usersPractice[index];
+    this.myRecForm.setValue({
+      'name': userData.name,
+      'technology': userData.technology
+    });
+  }
+
+  // use delete data:-
+  myDeleteData(userId: any) {
+    this._serverService.onDeleteData(userId).subscribe(
+      (res) => {
+        this.myGetData();
+      },
+      (err) => { }
+    );
   }
 
 }
