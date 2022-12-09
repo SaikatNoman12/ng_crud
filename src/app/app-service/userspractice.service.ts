040bebf75dea5d2e9cce379d03181c4a00cde330
saikatnoman12 <noommaa240@gmail.com>
2022-12-09 12:20:58 +0600

    add post method

--- src/app/app-service/userspractice.service.ts
+++ src/app/app-service/userspractice.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserPractice } from '../interface/user-practice.modal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserspracticeService {
 
-  constructor() { }
+  postUrl: string = 'https://practice-6b131-default-rtdb.asia-southeast1.firebasedatabase.app/usersPractice.json';
+
+  constructor(private http: HttpClient) { }
+
+  onSetData(userData: any) {
+    return this.http.post<UserPractice>(this.postUrl, userData);
+  }
+
+
 }
--- src/app/manage-user-practice/manage-user-practice.component.ts
+++ src/app/manage-user-practice/manage-user-practice.component.ts
@@ -1,6 +1,7 @@
 import { User } from './../interface/user.modal';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserspracticeService } from '../app-service/userspractice.service';
 
 @Component({
   selector: 'app-manage-user-practice',
@@ -9,25 +10,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ManageUserPracticeComponent implements OnInit {
 
-  myRecForm!:FormGroup;
+  myRecForm!: FormGroup;
 
-  constructor() { }
+  usersPractice:any[] = [];
+
+  constructor(
+    private _serverService: UserspracticeService
+  ) { }
 
   ngOnInit(): void {
     this.myRecForm = new FormGroup({
-      'name':new FormControl(null, [Validators.required]),
-      'technology':new FormControl(null, [Validators.required])
+      'name': new FormControl(null, [Validators.required]),
+      'technology': new FormControl(null, [Validators.required])
     });
   }
 
   // use for short validation:-
-  get fc(){
+  get fc() {
     return this.myRecForm.controls;
   }
 
-  myRecFromSubmit(){
-    const data:User = this.myRecForm.value;
-    
+  myRecFromSubmit() {
+
+
+    if (this.fc?.['name']?.value === null || this.fc?.['name']?.value === '' && this.fc?.['technology']?.value === null || this.fc?.['technology']?.value === '') {
+      alert('please enter all input value!');
+    }
+    else if(this.fc?.['name']?.value === null || this.fc?.['name']?.value === ''){
+      alert('Please enter name input value!');
+    }
+    else if(this.fc?.['technology'].value === null || this.fc?.['technology']?.value === ''){
+      alert('Please enter technology input value!');
+    }
+    else {
+      const data: User = this.myRecForm.value;
+      this.usersPractice.push(data);
+      this._serverService.onSetData(data).subscribe(
+        (res) => {
+
+         },
+        (err) => { }
+      );
+      this.myRecForm.reset();
+      console.log(this.myRecForm);
+
+    }
+
   }
 
 }
